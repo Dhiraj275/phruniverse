c1963abd0bb240b5b054f7df6a7abcd628df6903
Dhiraj275 <rp170854@gmail.com>
2024-02-15 19:36:11 +0530

    Gallery page added

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+
+function toggleNav() {
+    const navbar = document.querySelector(".navbar")
+    if (navbar.classList.contains("active")) {
+        navbar.classList.remove("active")
+    } else {
+        navbar.classList.add("active")
+    }
+}
